d79f2828616b8f80865b509497f7d64592a0116e
Vlad Vovk <wolfykey@gmail.com>
2020-03-31 23:10:29 +0300

    os module

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os')
+
+// platform
+console.log(os.platform())
+
+// architecture
+console.log(os.arch())
+
+// cpu
+console.log(os.cpus())
+
+// free ram memory
+console.log(os.freemem())
+
+// all ram memory
+console.log(os.totalmem())
+
+// home directory
+console.log(os.homedir())
+
+// device work time
+console.log(os.uptime())
